4bd5a53798df64e295466d523ed97b5c68eb002b
Raviverma Chamarti <raviverma.chamarti@gmail.com>
2023-04-16 05:20:30 +0000

    Adding routes and corresponding html files

--- Wallmart_Online_Store.py
+++ Wallmart_Online_Store.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route("/home")
+def home():
+    return render_template("home.html", title="home")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="login")
+
+
+@app.route("/register")
+def sign_up():
+    return render_template("register.html", title="register")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("invalidUser.html", title="logout")
+
+
+@app.route("/store")
+def news():
+    return render_template("store.html", title="store")
+
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", title="admin")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title="profile")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
