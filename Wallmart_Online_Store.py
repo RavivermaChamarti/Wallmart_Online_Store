aeebb8df8b8a59f78019d69b16a822963ef9280e
Raviverma Chamarti <raviverma.chamarti@gmail.com>
2023-04-16 20:44:16 +0000

    Adding essential routes and display products on webpage

--- Wallmart_Online_Store.py
+++ Wallmart_Online_Store.py
@@ -1,6 +1,32 @@
+import os
+
+import psycopg2
 from flask import Flask, render_template
+from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
+
+import contextlib
+
+
+@contextlib.contextmanager
+def open_db():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="wallmart_store_db",
+        user=os.environ["DB_USERNAME"],
+        password=os.environ["DB_PASSWORD"]
+        )
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    yield cur
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @app.route("/")
 @app.route("/home")
@@ -13,7 +39,7 @@ def login():
 
 
 @app.route("/register")
-def sign_up():
+def register():
     return render_template("register.html", title="register")
 
 
@@ -23,8 +49,17 @@ def logout():
 
 
 @app.route("/store")
-def news():
-    return render_template("store.html", title="store")
+def store():
+    with open_db() as cur:
+        cur.execute("SELECT SKU, category_id, title, url FROM products")
+        products = cur.fetchall()
+    products_list = []
+    for row in products:
+        products_list.append(dict(row))
+
+    print(len(products_list))
+
+    return render_template("store.html", title="store", products=products_list)
 
 
 @app.route("/admin")
