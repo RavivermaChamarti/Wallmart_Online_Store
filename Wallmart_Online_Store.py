558f1bc6ec4020819a99986728b71ccc623db9b7
Raviverma Chamarti <raviverma.chamarti@gmail.com>
2023-04-18 15:53:19 +0000

    Adding validation for registration

--- Wallmart_Online_Store.py
+++ Wallmart_Online_Store.py
@@ -51,39 +51,21 @@ def register():
     form = RegistrationForms()
     if form.validate_on_submit():
         with open_db() as cur:
-            # Check if username exists in database
-            cur.execute(f"""SELECT *
-                            FROM credentials
-                            WHERE username='{form.username.data}'""")
-            duplicate_username = cur.fetchone()
-
-            # Check if email id exists in database
-            cur.execute(f"""SELECT email_id
+            cur.execute(f"""INSERT INTO persons(first_name, last_name, email_id)
+                            VALUES ('{form.first_name.data}','{form.last_name.data}', '{form.email_id.data}' )""")
+            cur.execute(f"""SELECT person_id
                             FROM persons
-                            WHERE email_id='{form.email_id.data}'""")
-            duplicate_email_id = cur.fetchone()
-
-            if not duplicate_username:
-                if not duplicate_email_id:
-                    cur.execute(f"""INSERT INTO persons(first_name, last_name, email_id)
-                                    VALUES ('{form.first_name.data}','{form.last_name.data}', '{form.email_id.data}' )""")
-                    cur.execute(f"""SELECT person_id
-                                    FROM persons
-                                    WHERE first_name='{form.first_name.data}'
-                                            AND last_name='{form.last_name.data}'
-                                            AND email_id='{form.email_id.data}'""")
-                    user = cur.fetchone()
-                    person_id = user['person_id']
-                    salt = token_hex(16)
-                    password_hash = bcrypt.generate_password_hash(salt+form.password.data).decode("utf-8")
-                    cur.execute(f"""INSERT INTO credentials(username, person_iD, salt, password_hash)
-                                    VALUES ('{form.username.data}','{person_id}', '{salt}', '{password_hash}' )""")
-                    flash(f"Account Created for {form.first_name.data} {form.last_name.data}!", "success")
-                    return redirect(url_for('store'))
-                else:
-                    flash(f"There is an accounted linked to {form.email_id.data}! Please Log in if you already have an account", "danger")
-            else:
-                flash(f"{form.username.data} is taken! Please Choose a different username", "danger")
+                            WHERE first_name='{form.first_name.data}'
+                                    AND last_name='{form.last_name.data}'
+                                    AND email_id='{form.email_id.data}'""")
+            user = cur.fetchone()
+            person_id = user['person_id']
+            salt = token_hex(16)
+            password_hash = bcrypt.generate_password_hash(salt+form.password.data).decode("utf-8")
+            cur.execute(f"""INSERT INTO credentials(username, person_iD, salt, password_hash)
+                            VALUES ('{form.username.data}','{person_id}', '{salt}', '{password_hash}' )""")
+            flash(f"Account Created for {form.first_name.data} {form.last_name.data}!", "success")
+            return redirect(url_for('store'))
     return render_template("register.html", title="Register", form=form)
 
 
